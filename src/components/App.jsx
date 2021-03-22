d790b5521bd37112e89c89759a33b8d4a4bcd4f1
jacoblimzm <jacob_lim@live.com>
2021-03-22 20:14:25 +0800

    create filmtrek app with create-react-app

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import "../App.css";
+import axios from "axios";
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
