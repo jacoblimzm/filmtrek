e57d2b5509733618456951e44683ecdd7b368209
jacoblimzm <jacob_lim@live.com>
2021-03-25 18:10:16 +0800

    create Nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,60 @@
+import { Link } from "react-router-dom"
+
+const Nav = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <div className="container-fluid ">
+        <a className="navbar-brand" href="#">
+          filmtrek
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarToggler"
+          aria-controls="navbarToggler"
+          aria-expanded="false"
+          aria-label="toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarToggler"
+        >
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Results
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#" tabIndex="-1">
+                Movies
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-light" type="submit">
+              Find
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+
+export default Nav
