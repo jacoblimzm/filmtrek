bfd6b0b033f126d611b4c32d221b1f025edca3c0
jacoblimzm <jacob_lim@live.com>
2021-03-25 17:29:29 +0800

    shift youtube API call to testCode

--- src/testCode.js
+++ src/testCode.js
@@ -16,3 +16,15 @@ const getSpotifyTokenRegular = () => {
 // Spotify Endpoints:
 // Get All Categories: https://api.spotify.com/v1/browse/categories
 // 
+
+// ===== Youtube Video Search API Call
+const getYoutubeSearchResults = async () => {
+  const YTapi = process.env.REACT_APP_YT_API_KEY;
+  const searchEP = "https://www.googleapis.com/youtube/v3/search";
+
+  const response = await axios.get(
+    `https://www.googleapis.com/youtube/v3/search?q=interstellar%20album&part=snippet&type=video&key=${YTapi}&maxResults=10&videoEmbeddable=true`
+  );
+
+  console.log(response.data);
+};
