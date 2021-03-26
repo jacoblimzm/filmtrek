0092c6b52fa0f3641dbeeb8ab1b42d96146f4a69
jacoblimzm <jacob_lim@live.com>
2021-03-26 16:04:46 +0800

    provide static data to ResultsPage component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,7 @@ import Nav from "./Nav";
 import LandingPage from "./LandingPage";
 import FilmItem from "./FilmItem";
 import ResultsPage from "./ResultsPage";
+import DetailsPage from "./DetailsPage";
 
 dotenv.config();
 
@@ -79,59 +80,8 @@ function App() {
         <Nav />
         {/* <LandingPage handleUserSearch={handleUserSearch} popFilms={popFilms}/> */}
         {/* <ResultsPage films={popFilms}/> */}
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-6">
-              <h2>DETAILS</h2>
-              <div className="film-detail">
-                <div className="film-backdrop">
-                  <img
-                    className="img-fluid"
-                    src="https://image.tmdb.org/t/p/w1280/hJuDvwzS0SPlsE6MNFOpznQltDZ.jpg"
-                  />
-                  <h1 className="film-title">Title of Film (2007)</h1>
-                </div>
-                <div className="film-meta">
-                  <img
-                    className="film-meta-poster"
-                    src="https://image.tmdb.org/t/p/w780//lPsD10PP4rgUGiGR4CCXA6iY0QQ.jpg"
-                  />
-                  <h2>Tagline</h2>          
-                  <p>227mins</p>
-                  <p>Rating: 8.5/10</p>
-                  <p>Overview</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-6">
-              <h2>Selected Album</h2>
-              <div className="row album-details">
-                <div className="col-12">
-                  <img
-                    className="img-fluid"
-                    src={searchAlbums[0].images[1].url}
-                  />
-                </div>
-                <div className="col-12">
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">An item</li>
-                    <li class="list-group-item">A second item</li>
-                    <li class="list-group-item">A third item</li>
-                    <li class="list-group-item">A fourth item</li>
-                    <li class="list-group-item">And a fifth one</li>
-                    <li class="list-group-item">An item</li>
-                    <li class="list-group-item">A second item</li>
-                    <li class="list-group-item">A third item</li>
-                    <li class="list-group-item">A fourth item</li>
-                    <li class="list-group-item">And a fifth one</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
+        <DetailsPage />
         </div>
-      </div>
     </Router>
   );
 }
--- src/components/ResultsPage.jsx
+++ src/components/ResultsPage.jsx
@@ -1,79 +1,79 @@
+import FilmItem from "./FilmItem"
+
+const ResultsPage = ( {films} ) => {
+
+    const filmItems = films.map((film) => {
+        return <FilmItem film={film} />;
+      });
 
 
-// const ResultsPage = ( {films} ) => {
-
-//     const filmItems = films.map((film) => {
-//         return <FilmItem film={film} />;
-//       });
-
-
-//     return (
-//         <div className="results-page container-fluid ">
-//           <h1>RESULTS</h1>
-//           <div className="row">
-//             <div className="col-md-6">
-//               <h1>FILMS</h1>
-//               <div className="film-search-results row">{films}</div>
-//             </div>
-//             <div className="col-md-6">
-//               <div className="row">
-//                 <div className="col-12">
-//                   <h1>ALBUMS</h1>
-//                   <div className="album-search-results row">
-//                     <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
-//                       <img src={searchAlbums[0].images[1].url} />
-//                     </div>
-//                     <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
-//                       <img src={searchAlbums[0].images[1].url} />
-//                     </div>
-//                     <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
-//                       <img src={searchAlbums[0].images[1].url} />
-//                     </div>
-//                     <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
-//                       <img src={searchAlbums[0].images[1].url} />
-//                     </div>
-//                     <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
-//                       <img src={searchAlbums[0].images[1].url} />
-//                     </div>
-//                     <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
-//                       <img src={searchAlbums[0].images[1].url} />
-//                     </div>
-//                     <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
-//                       <img src={searchAlbums[0].images[1].url} />
-//                     </div>
-//                     <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
-//                       <img src={searchAlbums[0].images[1].url} />
-//                     </div>
-//                   </div>
-//                 </div>
-//                 <div className="col-12">
-//                   <h2>Selected Album</h2>
-//                   <div className="row selected-album">
-//                     <div className="col-sm-12 col-lg-3">
-//                       <img src={searchAlbums[0].images[1].url} />
-//                     </div>
-//                     <div className="col-sm-12 col-lg-9">
-//                       <ul class="list-group list-group-flush">
-//                         <li class="list-group-item">An item</li>
-//                         <li class="list-group-item">A second item</li>
-//                         <li class="list-group-item">A third item</li>
-//                         <li class="list-group-item">A fourth item</li>
-//                         <li class="list-group-item">And a fifth one</li>
-//                         <li class="list-group-item">An item</li>
-//                         <li class="list-group-item">A second item</li>
-//                         <li class="list-group-item">A third item</li>
-//                         <li class="list-group-item">A fourth item</li>
-//                         <li class="list-group-item">And a fifth one</li>
-//                       </ul>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//     )
-// }
+    return (
+        <div className="results-page container-fluid ">
+          <h1>RESULTS</h1>
+          <div className="row">
+            <div className="col-md-6">
+              <h1>FILMS</h1>
+              <div className="film-search-results row">{filmItems}</div>
+            </div>
+            <div className="col-md-6">
+              <div className="row">
+                <div className="col-12">
+                  <h1>ALBUMS</h1>
+                  <div className="album-search-results row">
+                    <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
+                      <img src="https://i.scdn.co/image/ab67616d0000b273ac29a65e7ffcfa6f9cb0d342" />
+                    </div>
+                    <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
+                      <img src="https://i.scdn.co/image/ab67616d0000b273ac29a65e7ffcfa6f9cb0d342" />
+                    </div>
+                    <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
+                      <img src="https://i.scdn.co/image/ab67616d0000b273ac29a65e7ffcfa6f9cb0d342" />
+                    </div>
+                    <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
+                      <img src="https://i.scdn.co/image/ab67616d0000b273ac29a65e7ffcfa6f9cb0d342" />
+                    </div>
+                    <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
+                      <img src="https://i.scdn.co/image/ab67616d0000b273ac29a65e7ffcfa6f9cb0d342" />
+                    </div>
+                    <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
+                      <img src="https://i.scdn.co/image/ab67616d0000b273ac29a65e7ffcfa6f9cb0d342" />
+                    </div>
+                    <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
+                      <img src="https://i.scdn.co/image/ab67616d0000b273ac29a65e7ffcfa6f9cb0d342" />
+                    </div>
+                    <div className="col-sm-6 col-md-6 col-lg-4 pb-5">
+                      <img src="https://i.scdn.co/image/ab67616d0000b273ac29a65e7ffcfa6f9cb0d342" />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-12">
+                  <h2>Selected Album</h2>
+                  <div className="row selected-album">
+                    <div className="col-sm-12 col-lg-3">
+                      <img src="https://i.scdn.co/image/ab67616d0000b273ac29a65e7ffcfa6f9cb0d342" />
+                    </div>
+                    <div className="col-sm-12 col-lg-9">
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                        <li class="list-group-item">A fourth item</li>
+                        <li class="list-group-item">And a fifth one</li>
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                        <li class="list-group-item">A fourth item</li>
+                        <li class="list-group-item">And a fifth one</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    )
+}
 
 
-// export default ResultsPage
+export default ResultsPage
