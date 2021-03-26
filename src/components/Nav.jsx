aac1ac50c942740c8e4d978b5ea30a16f8aef20b
jacoblimzm <jacob_lim@live.com>
2021-03-26 16:21:22 +0800

    Nav linked to page components with react router

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
       <div className="container-fluid ">
-        <a className="navbar-brand" href="#">
+        <Link to="/" className="navbar-brand">
           filmtrek
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -22,26 +22,26 @@ const Nav = () => {
           className="collapse navbar-collapse justify-content-end"
           id="navbarToggler"
         >
-          <ul className="navbar-nav mb-2 mb-lg-0">
+          <ul className="navbar-nav mb-2 mb-lg-0 mx-4">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
+              <Link to="/" className="nav-link active" aria-current="page">
+                home
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Results
-              </a>
+              <Link to="/results" className="nav-link active">
+                results
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#" tabIndex="-1">
-                Details
-              </a>
+              <Link to="/details" className="nav-link active" tabIndex="-1">
+                details
+              </Link>
             </li>
           </ul>
           <form className="d-flex">
             <input
-              className="form-control me-2"
+              className="form-control me-2 mx-2"
               type="search"
               placeholder="Search"
               aria-label="Search"
@@ -56,5 +56,4 @@ const Nav = () => {
   );
 };
 
-
-export default Nav
+export default Nav;
