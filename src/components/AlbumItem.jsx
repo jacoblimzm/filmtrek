c317b0a95be8dca6b773adc0bcbf96bd9298c943
jacoblimzm <jacob_lim@live.com>
2021-03-30 23:35:48 +0800

    clean up css styling

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -4,7 +4,7 @@ const AlbumItem = ({ images, name, id }) => {
   return (
     <div className="album-item hvr-grow-shadow col-sm-6 col-md-6 col-lg-4 my-3 px-2">
       <Link to={`/albumdetails/${id}`}>
-        <img src={images[0].url} alt={name} />
+        <img className="box-shadow" src={images[0].url} alt={name} />
       </Link>
     </div>
   );
